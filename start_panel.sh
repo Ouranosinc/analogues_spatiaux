b551e3c1a1e6dd8b0c6f9badc5bd4bc7ab17e192
Sarah G <91751417+sg2475962@users.noreply.github.com>
2023-08-29 15:56:28 -0400

    Max processors out

--- start_panel.sh
+++ start_panel.sh
@@ -9,6 +9,7 @@ LOG_LEVEL="debug"
 MAX_PROCS=$(nproc --all)
 
 NUM_PROCS=$((MAX_PROCS - 1 > 16 ? 16 : MAX_PROCS - 1))
+NUM_PROCS=0
 NUM_THREADS=0
 
 panel serve "${DASHBOARD_FILE}" --setup "${SETUP_FILE}" --session-token-expiration "${SESSION_TOKEN_EXPIRATION}" --prefix "${PREFIX}" --use-xheaders --log-level="${LOG_LEVEL}" --static-dirs fonts=./fonts scripts=./scripts --num-procs $NUM_PROCS --num-threads $NUM_THREADS
