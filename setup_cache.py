d027cb50cc19faa69bedc823a4ffbb3479e12866
Xavier Frenette <xavier.frenette@crim.ca>
2024-05-30 09:55:15 -0400

    Fix: Setup is failing and preventing the app from starting

--- setup_cache.py
+++ setup_cache.py
@@ -1,16 +1,39 @@
 # .py file for warming up the dashboard cache
+import logging
+
 import panel as pn
-import core
-import xclim
+
 from core import utils
+
+logger = logging.getLogger('analogs-setup')
+logger.setLevel(logging.INFO)
+
+logger.info("Cache warming is starting")
 
 utils.check_version_delete_cache()
 
+logger.info("Loading: config")
 config = pn.state.as_cached('config',utils.load_config)
+
+logger.info("Loading: datavars")
 datavars = pn.state.as_cached('datavars',utils.load_datavars)
+
+logger.info("Loading: cities")
 cities = pn.state.as_cached('cities',utils.load_cities)
+
+logger.info("Loading: places")
 places = pn.state.as_cached('places',utils.load_places)
+
+logger.info("Loading: dref")
 dref = pn.state.as_cached('dref',utils.load_dref)
+
+logger.info("Loading: dsim")
 dsim = pn.state.as_cached('dsim',utils.load_dsim)
+
+logger.info("Loading: density")
 density = pn.state.as_cached('density',utils.load_density)
+
+logger.info("Loading: benchmark")
 benchmark = pn.state.as_cached('benchmark',utils.load_benchmark)
+
+logger.info("Cache warming done. Setup finished.")
